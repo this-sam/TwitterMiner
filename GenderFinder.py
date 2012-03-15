d3b0bf13f00d832e77f06a2ae333b664cc070eaa
this-sam <sbbrown@wapiti.uvm.edu>
2012-03-15 13:58:46 -0400

    Implemented hash table for gender matcher.

--- GenderFinder.py
+++ GenderFinder.py
@@ -1,2 +1,59 @@
-class GenderFinder():
-	pass
+class GenderFinder(object):
+	
+	global Settings
+	from Settings import Settings
+	
+	def __init__(self):
+		#load names
+		self.maleNames = open(Settings.MALE_FILE, 'r').readlines()
+		self.femaleNames = open(Settings.FEMALE_FILE, 'r').readlines()
+		
+		#clean them up
+		for i in range(len(self.maleNames)):
+			self.maleNames[i] = self.maleNames[i].strip()
+		
+		for i in range(len(self.femaleNames)):
+			self.femaleNames[i] = self.femaleNames[i].strip()
+		
+		#load the names into hash table into memory
+		self.maleHash = self.__generateHashTable(self.maleNames)
+		self.femaleHash = self.__generateHashTable(self.femaleNames)
+		
+		print self.maleHash
+		print self.femaleHash
+	
+	def lookupGender(self, name):
+		"""Determine which gender a name belongs to.  Return M for male, F for
+		female and U for undetermined."""
+		
+		#if it's too short to look up, don't bother
+		if len(name)<2:
+			return 'U'
+		
+		#first check male list
+		isMale = False
+		
+		
+		
+	
+	def __generateHashTable(self, words):
+		hash = {}
+		for i in range(len(words)):
+			#if you're not 2 letters long, i don't care about you.
+			if len(words[i]) < 2:
+				break
+			
+			#use the first two letters of each name to speed up lookups
+			key = words[i][0:2]
+			
+			#build a hash table of char1-->char2-->index in array
+			if key[0] not in hash:
+				hash[key[0]] = {key[1]:i}
+			elif key[1] not in hash[key[0]]:
+				hash[key[0]][key[1]] = i
+		
+		return hash
+				
+	
+if __name__ == '__main__':
+	gf = GenderFinder()
--- Settings.py
+++ Settings.py
@@ -6,11 +6,17 @@ class Settings:
      #----------File Location Constants----------
      #ROOT_DIR = "/home/sam/Development/TwitterMiner/Tweets"
      #mac
-     ROOT_DIR = "/Volumes/Macintosh HD 2/Development/Thesis/Twitter/Tweets"
-     SELF_DIR = ROOT_DIR.rsplit("/",1)[0]+"/"
+     #ROOT_DIR = "/Volumes/Macintosh HD 2/Development/Thesis/Twitter/Tweets"
      #uvm srvr
-     #ROOT_DIR = '/users/s/b/sbbrown/Development/Thesis/Chat/Files/'
+     ROOT_DIR = '/users/s/b/sbbrown/Development/Thesis/TwitterMiner/'
      #CsCrew Mac
      #ROOT_DIR = '/Users/cscrew/Thesis/Files/'
      #PC
      #ROOT_DIR = "E:\\Development\\Thesis\\Twitter\\Tweets\\"
+     
+     #calculate where this file is from the directory of tweets
+     SELF_DIR = ROOT_DIR.rsplit("/",1)[0]+"/"
+     
+     #male/female name lists
+     MALE_FILE = SELF_DIR+"Resources/finalMale.txt"
+     FEMALE_FILE = SELF_DIR+"Resources/finalFemale.txt"    
